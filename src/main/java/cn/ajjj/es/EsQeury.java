91580f136e17a3a7643313c9f98b1f665fa9d4c7
yangshuofu <yangshuofu@anji-allways.com>
2020-01-14 09:14:59 +0800

    过滤和横向越权拦截

--- src/main/java/cn/ajjj/es/EsQeury.java
+++ src/main/java/cn/ajjj/es/EsQeury.java
@@ -1,6 +1,8 @@
 package cn.ajjj.es;
 
+import cn.ajjj.redis.RedisSimpleUtil;
 import cn.ajjj.util.DateTime;
+import com.sun.deploy.util.StringUtils;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -11,6 +13,8 @@ import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.index.query.RangeQueryBuilder;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.mortbay.util.StringUtil;
+import redis.clients.jedis.JedisCluster;
 
 import java.io.IOException;
 import java.text.ParseException;
@@ -24,36 +28,75 @@ import static org.elasticsearch.index.query.QueryBuilders.matchQuery;
 public class EsQeury {
 
     private  PollingBulkProcessor pollingBulkProcessor;
-    //query 当天0点到目前 符合条件的数据
 
-    public static List query(RestHighLevelClient client,String indexname, String type, String field, String filedtype) throws ParseException {
+
+    //第一次查询的脱敏数据
+    public static List query(JedisCluster jedisCluster,RestHighLevelClient client,String indexname, String type, String field, String filedtype) throws ParseException {
         List result = new ArrayList();
+
         Map<String, Object> sourceAsMap = new HashMap<>();
         try {
-                if (ESUtil.indexExists(client, indexname)){
-                    SearchRequest searchRequest = new SearchRequest(indexname);
-                    SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-                    BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery().
-                            must(matchQuery(filedtype, field));
-                    searchSourceBuilder.query(boolQueryBuilder);
-                    searchRequest.source(searchSourceBuilder);
-                    SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);//同步执行
-                    System.out.println("!!"+searchResponse);
-                    SearchHits hits = searchResponse.getHits();
-                    if (!hits.equals("") && hits !=null) {
-                        for (int i = 0; i < hits.getHits().length; i++) {
-                            sourceAsMap = hits.getHits()[i].getSourceAsMap();
-                            result.add(sourceAsMap.get("message"));
-                            System.out.println(sourceAsMap.get("message") );
-                        }
+            if (ESUtil.indexExists(client, indexname)){
+                SearchRequest searchRequest = new SearchRequest(indexname);
+                SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+                BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery().
+                        must(matchQuery(filedtype, field));
+                searchSourceBuilder.query(boolQueryBuilder);
+                searchRequest.source(searchSourceBuilder);
+                SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);//同步执行
+                System.out.println("!!"+searchResponse);
+                SearchHits hits = searchResponse.getHits();
+                if (!hits.equals("") && hits !=null) {
+                    for (int i = 0; i < hits.getHits().length; i++) {
+                        sourceAsMap = hits.getHits()[i].getSourceAsMap();
+                        result.add(sourceAsMap);
+                        System.out.println(sourceAsMap.get("event_id") );
+                        jedisCluster.set(field,sourceAsMap.get("event_id").toString());
+                        jedisCluster.expire(field,3000);
                     }
-                    return result;
                 }
+                return result;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return  result;
     }
 
+    //第二次查询的原始数据
+    public static List query_data(JedisCluster jedisCluster,RestHighLevelClient client,String indexname, String type, String field) throws ParseException {
+        List result = new ArrayList();
+
+        Map<String, Object> sourceAsMap = new HashMap<>();
+        String event_id = jedisCluster.get(field);
+        if (event_id!=null&& !event_id.equals("")){
+            try {
+                if (ESUtil.indexExists(client, indexname)) {
+                    SearchRequest searchRequest = new SearchRequest(indexname);
+                    SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+                    BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery().
+                            must(matchQuery("event_id", event_id));
+                    searchSourceBuilder.query(boolQueryBuilder);
+                    searchRequest.source(searchSourceBuilder);
+                    SearchResponse searchResponse = client.search(searchRequest, RequestOptions.DEFAULT);//同步执行
+                    System.out.println("!!" + searchResponse);
+                    SearchHits hits = searchResponse.getHits();
+                    if (!hits.equals("") && hits != null) {
+                        for (int i = 0; i < hits.getHits().length; i++) {
+                            sourceAsMap = hits.getHits()[i].getSourceAsMap();
+                            result.add(sourceAsMap);
+                        }
+                    }
+                    return result;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        return result;
+
+    }
 
 }
--- src/main/java/cn/ajjj/es/PollingBulkProcessor.java
+++ src/main/java/cn/ajjj/es/PollingBulkProcessor.java
@@ -1,6 +1,9 @@
 package cn.ajjj.es;
 
+import cn.ajjj.redis.RedisSimpleUtil;
 import org.elasticsearch.client.RestHighLevelClient;
+import redis.clients.jedis.JedisCluster;
+
 import java.text.ParseException;
 import java.util.List;
 
@@ -10,13 +13,31 @@ import java.util.List;
  */
 public class PollingBulkProcessor {
     public RestHighLevelClient client;
+    private static JedisCluster jedisCluster;
+    static {
+        jedisCluster = RedisSimpleUtil.getInstance();
+    }
     public void judageAndCreateIndexName(String indexName) {
         ESUtil.judgeAndCreateIndex(client, indexName);
     }
 
-    public  List query(String indexname, String type, String field, String filedtype) throws ParseException {
-        List query = EsQeury.query(client, indexname, type, field, filedtype);
+    public  List query(String indexname, String type, String field, String filedtype,String cookid,String userid) throws ParseException {
+        //保存登录的用户和cookid，又来防止横向越权操作
+        jedisCluster.set(cookid.toString(), userid.toString());
+        jedisCluster.expire(cookid.toString(),1800);
+        List query = EsQeury.query(jedisCluster,client, indexname, type, field, filedtype);
         return query;
+    }
+    public  List query_data(String indexname, String type, String field, String filedtype,String cookid,String userid) throws ParseException {
+        //判断用户id与上次传过来的是否一致
+        //保存登录的用户和cookid，又来防止横向越权操作
+        String uid=jedisCluster.get(cookid.toString());
+        if (uid.equals(userid)) {
+            List query = EsQeury.query_data(jedisCluster,client, indexname, type, field);
+            return query;
+        }else {
+            return null;
+        }
     }
 
     public PollingBulkProcessor() {
